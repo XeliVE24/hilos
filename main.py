ac27c4414823fc658e86c3607c2f47da1cd3400a
Xeli Vargas <xeli.vargas@uteca.edu.mx>
2024-04-04 07:58:36 -0600

    hilos

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ print("________________________________________________________________________"
 
 print("usando 2 hilos")
 print() 
+
 globalArrayNum=[]
 def contadorDos(inicio,fin):
     for i in range(inicio,fin+1,1):
@@ -63,17 +64,37 @@ t.start()
 t=threading.Thread(target=contadorDos,args=(76,100))
 listaHilos.append(t)
 t.start()
-"""
-for i in range (4):
-    t=threading.Thread(target=contadorDos,args=(100/4))
-    listaHilos.append(t)
-    t.start()"""
 
-for t in listaHilos:
+#"----------------------------")
+ArrayNum=[]
+def contador3(inicio,fin):
+    for i in range(inicio,fin+1,1):
+        ArrayNum.append(i)
+        time.sleep(0.01)
+    return 0
+
+t0=time.time()
+listHilos=[]
+#numhilos=4
+
+rangos=[(1,25),(26,50),(51,75),(76,100)]
+for i ,rango in enumerate (rangos):
+   # inicio=1
+   # fin=inicio+(rango/numhilos) -1 if i < numhilos -1 else rango 
+    t=threading.Thread(target=contador3,args=(rango))#(inicio,fin)
+    listHilos.append(t)
+    t.start()
+    #inicio=fin
+
+for t in listHilos:
     t.join()
+
 
 tf=time.time()-t0
 print(f"tiempo total de ejecucion:{tf}")
 print(globalArrayNum)
 
-
+print("----------------------------")
+tf=time.time()-t0
+print(f"tiempo total de ejecucion:{tf}")
+print(ArrayNum)
